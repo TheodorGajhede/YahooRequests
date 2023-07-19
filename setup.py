875e2c635c03ec519759f663f1c671b25fa0a56a
Theodor.Gajhede <theodorraabjerggajhede@gmail.com>
2023-07-19 15:29:26 +0200

    new file:   dist/YahooRequests-0.1.1.tar.gz 	modified:   dist/YahooRequests-0.1.tar.gz 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(
-    name='YahooRequests',
+    name='YahooRequest',
     packages=['YahooRequests'],
-    version='0.1',
+    version='0.1.1',
     license='MIT',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -10,7 +10,7 @@ setup(
     author='Theodor Gajhede',
     author_email='theodorgajhede@gmail.com',
     url='https://github.com/TheodorGajhede/YahooRequests',
-    download_url='https://github.com/TheodorGajhede/YahooRequests/archive/refs/tags/TEST.tar.gz',
+    download_url='https://github.com/TheodorGajhede/YahooRequests/archive/refs/tags/BETA.tar.gz',
     keywords=['Stocks', 'Ticker', 'Yahoo'],
     install_requires=[
           'requests'
